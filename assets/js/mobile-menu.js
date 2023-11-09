184ba41132c5301f5ebbda2808e41010f2a25e0c
Paweł Szopiński <contact@pawelszopinski.com>
2023-11-09 19:18:09 +0100

    Add mobile menu.
    Mobile menu is visible when viewport width <= 400px.

---
+++ assets/js/mobile-menu.js
@@ -0,0 +1,7 @@
+document.getElementById("open-mobile-menu").onclick = () => {
+  document.body.classList.add("mobile-menu-shown");
+};
+
+document.getElementById("close-mobile-menu").onclick = () => {
+  document.body.classList.remove("mobile-menu-shown");
+};
